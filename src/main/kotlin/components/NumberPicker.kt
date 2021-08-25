3d9e41cc999940a3a95190c9a5eec5d6359e5836
Sebastian <sebastianliando@gmail.com>
2021-08-26 01:38:45 +0800

    Flatten text field

--- src/main/kotlin/components/NumberPicker.kt
+++ src/main/kotlin/components/NumberPicker.kt
@@ -1,7 +1,10 @@
 package components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,9 +13,13 @@ import androidx.compose.material.icons.rounded.Remove
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.math.max
 import kotlin.math.min
 
@@ -21,18 +28,19 @@ fun NumberPicker(
     current: Int?,
     max: Int,
     min: Int = 0,
-    onChanged: (Int?) -> Unit
+    onChanged: (Int?) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         IconButton(
             onClick = {
                 onChanged((current?.minus(1) ?: min).coerceAtLeast(0))
             }
         ) {
-            Icon(Icons.Rounded.Remove, "Reduce one")
+            Icon(Icons.Rounded.Remove, "Reduce one", tint = MaterialTheme.colors.primary)
         }
 
-        OutlinedTextField(
+        BasicTextField(
             value = current?.toString() ?: "",
             onValueChange = { value ->
                 if (value.isBlank()) {
@@ -43,10 +51,19 @@ fun NumberPicker(
                     }
                 }
             },
-            isError = current == null,
+//            isError = current == null,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.size(100.dp, 50.dp),
-            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+            modifier = Modifier.width(50.dp),
+            decorationBox = { textField ->
+                Box(
+                    modifier = Modifier
+                        .border(1.dp, Color.LightGray, shape = RoundedCornerShape(4.dp))
+                        .padding(vertical = 4.dp)
+                ) {
+                    textField()
+                }
+            },
+            textStyle = MaterialTheme.typography.caption.copy(textAlign = TextAlign.Center)
         )
 
         IconButton(
@@ -54,7 +71,7 @@ fun NumberPicker(
                 onChanged((current?.plus(1) ?: (min + 1)).coerceAtMost(max))
             }
         ) {
-            Icon(Icons.Rounded.Add, "Add one")
+            Icon(Icons.Rounded.Add, "Add one", tint = MaterialTheme.colors.primary)
         }
     }
 }
